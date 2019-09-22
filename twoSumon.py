467c0a8f81656b527328998d6306f6e2a0598c60
Daniel Waterfield <daniel.waterfield@gmail.com>
2019-09-22 16:11:23 +0100

    Update twoSumon.py

--- twoSumon.py
+++ twoSumon.py
@@ -18,3 +18,6 @@ def twoSum(a, target):
 a = [2,7,11,15, 17, 19]
 target = 9
 print(twoSum(a, target))
+
+#this is of course in o-n time, but the real issue is getting it to print the indices, 
+# which would require enumerate, but also reworking the entire code...
