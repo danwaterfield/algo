8345e2d0ec93571533a80bd515baf1e763687889
Daniel Waterfield <daniel.waterfield@gmail.com>
2019-10-30 14:51:14 +0000

    Create binarySearch.py

---
+++ binarySearch.py
@@ -0,0 +1,19 @@
+def binarySearch(list, value):
+	low = 0
+	high = len(list)-1 #last item in list, assumes this is a sorted list obviously!
+
+	while low <= high: 
+		mid = low + high
+		guess = list[mid]
+		if guess == value: #is the value the middle item in the list?  
+			return mid
+		if guess > value:
+			high = mid-1 #if the midpoint value is higher than the actual value, then the new high becomes one less than the midpoint of the list
+		else:
+			low = mid +1 # on the other hand, if the midpoint is lower than the value then the new low becomes one value higher than the midpoint.
+	return None
+
+l = list(range(1,100000))
+
+
+print binarySearch(l, 437)
